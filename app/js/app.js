3552bb341804833d702fc6b7c2a476af90795978
Segneanu Emilian <emilian.segneanu@yahoo.ro>
2017-01-14 18:37:11 +0000

    Final commit!

--- app/js/app.js
+++ app/js/app.js
@@ -1,30 +1,29 @@
 'use strict'
-const app=angular.module('moviesApp',[
+const app = angular.module('moviesApp', [
     'ui.router',
     'genreModule',
     'ngMessages'
-    ])
-    app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
-        $urlRouterProvider.otherwise('/genres')
-        $stateProvider
-            .state("home",{
-                url:'/home',
-                templateUrl:'views/home.html'
-            })
-            .state('genres',{
-                url:'/genres',
-                templateUrl:'views/genres.html',
-                controller:'genreController'
-            })
-            .state('genreMovies',{
-                url:'/genres/:genreId/movies',
-                templateUrl:'views/genresMovies.html',
-                controller:'genreMoviesController'
-            })
-            .state('movies',{
-                url:'/movies',
-                templateUrl:'views/movies.html',
-                controller:'movieController'
-            })
-    }])
-    
+])
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home')
+    $stateProvider
+        .state("home", {
+            url: '/home',
+            templateUrl: 'views/home.html'
+        })
+        .state('genres', {
+            url: '/genres',
+            templateUrl: 'views/genres.html',
+            controller: 'genreController'
+        })
+        .state('genreMovies', {
+            url: '/genres/:genreId/movies',
+            templateUrl: 'views/genresMovies.html',
+            controller: 'genreMoviesController'
+        })
+        .state('movies', {
+            url: '/movies',
+            templateUrl: 'views/movies.html',
+            controller: 'movieController'
+        })
+}])
